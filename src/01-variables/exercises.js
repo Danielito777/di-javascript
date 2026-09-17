aa836da588ea9333bb7a0b20ab0eae62f31b0abb
unknown <Daniel Torre>
2026-09-17 13:36:51 +0200

    Ejercicios del 1 al 4 de variables

---
+++ src/01-variables/exercises.js
@@ -0,0 +1,18 @@
+//1
+let nombre = "Daniel";
+console.log(nombre);
+
+//2
+const PI = 3.1416;
+//PI = 6;
+
+//3
+let edad;
+console.log(edad);
+edad = 18;
+console.log(edad);
+
+//4
+const PRECIO_BASE = 100;
+const IVA = 0.21;
+let precioFinal = PRECIO_BASE * (1 + IVA);
